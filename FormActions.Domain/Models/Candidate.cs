d7ac7b62c5250bc2f6a78f3180ca808d2cfc7f68
Ivana996 <50019358+IvanaInaN@users.noreply.github.com>
2022-09-03 15:27:37 +0200

    Refactoring

--- FormActions.Domain/Models/Candidate.cs
+++ FormActions.Domain/Models/Candidate.cs
@@ -6,8 +6,8 @@
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
-        public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
         public int Active { get; set; }
     }
 }
