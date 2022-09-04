25855e0cc9bd1db54fe70899055fb44d9c52a722
Ivana996 <50019358+IvanaInaN@users.noreply.github.com>
2022-09-05 01:40:39 +0200

    GetAllFormActionsQueryHandlerTest - Nothing test done

--- FormActions.UnitTests/Services/CQRS/Queries/GetAllFormActionsQueryHandlerTest.cs
+++ FormActions.UnitTests/Services/CQRS/Queries/GetAllFormActionsQueryHandlerTest.cs
@@ -1,11 +1,5 @@
-﻿using AutoMapper;
-using ComplexApp.Services.CQRS.Queries.GetAllFormActionsQuery;
-using FormActions.Domain.Models;
-using FormActions.Domain.Repositories;
+﻿using FluentAssertions;
 using FormActions.Services.CQRS.Queries.GetAllFormActionsQuery;
-using FormActions.Structures.Dtos;
-using Moq;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -13,43 +7,25 @@ using Xunit;
 namespace FormActions.UnitTests.Services.CQRS.Queries
 {
     public class GetAllFormActionsQueryHandlerTest
-    {
-        private readonly Mock<IFormActionRepository> _formActionRepository;
-        private readonly Mock<IMapper> _mapper;
+    {    
         private readonly GetAllFormActionsQueryHandler _handler;
 
         public GetAllFormActionsQueryHandlerTest()
         {
-            _formActionRepository = new Mock<IFormActionRepository>(MockBehavior.Strict);
-            _mapper = new Mock<IMapper>(MockBehavior.Strict);
-            _handler = new GetAllFormActionsQueryHandler(_formActionRepository.Object, _mapper.Object);
+            _handler = new GetAllFormActionsQueryHandler();
         }
 
         [Fact]
         public async Task GetAllFormActions()
         {
             // Arrange
-
             var request = new GetAllFormActionQueryRequest();
 
-            var result = new List<FormAction>();
-
-            var formActions = new List<FormActionDto>();
-
-            _formActionRepository.Setup(x => x.GetFormActionsAsync())
-                .Returns(Task.FromResult(result));
-
-            _mapper.Setup(x => x.Map<List<FormAction>, List<FormActionDto>>(result))
-                .Returns(formActions);
-
             // Act
-
             var response = await _handler.Handle(request, CancellationToken.None);
 
             // Assert 
-
-            _formActionRepository.Verify(x => x.GetFormActionsAsync(), Times.Once);
-            _mapper.VerifyAll();
+            response.Should().NotBeEmpty();
         }
     }
 }
